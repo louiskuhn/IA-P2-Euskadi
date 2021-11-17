4a9d2725c059ca140d0388e74d3ec9fb20e254be
louiskuhn <kuhnlouis@gmail.com>
2021-11-17 16:29:42 +0100

    régression linéaire simple implémentation

--- Régression linéaire simple/implementation_regression_lineaire_simple.py
+++ Régression linéaire simple/implementation_regression_lineaire_simple.py
@@ -1,31 +1,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+%matplotlib qt5
 
 # Charger les données, créer la matrice X et le vecteur y puis les afficher 
+#data = np.genfromtxt('salaires.csv', delimiter=',', skip_header=True)
+#x = data[:,:-1]
+#y = data[:,-1:]
+#n = len(y)
+
+# il est possible de générer des données aléatoires pour une régression
+from sklearn.datasets import make_regression
+x,y = make_regression(n_samples=100, n_features=1, noise = 10)
+y = y.reshape(y.shape[0],1)
 
 # Matrice X : oublier pas d'ajouter la colonne de 1 !
-X = 
+X = np.hstack((x,np.ones(x.shape)))
 
 # Initialisation theta
-theta = 
+theta = 100*np.random.randn(2,1)
 
 # Modèle
 def model(X,theta):
-    return 
+    return X @ theta
 
 # Fonction de coût
 def cout(X,y,theta):
-    return 
+    return 1/(2*n)*np.sum((model(X,theta)-y)**2)
+    #return 1/(2*n)*(model(X,theta)-y).T @ (model(X,theta)-y))
     
 # Calcul du gradient
 def grad(X,y,theta):
-    return 
+    return 1/n * X.T @ (model(X,theta)-y)
     
 # Descente de gradient
 def desc_grad(X,y,theta,alpha=0.01,n_max=1000):
-
+    couts = np.zeros(n_max)
+    for i in range(n_max):
+        theta = theta - alpha * grad(X,y,theta)
+        couts[i] = cout(X,y,theta)
+    return theta, couts
 
 # Solution exacte au problème
 def sol_exacte(X,y):
-    return
+    return np.linalg.inv(X.T @ X) @ X.T @ y
+
+# Optimisation : recherche du meilleur theta
+theta_exact = sol_exacte(X, y)
+theta_hat, couts = desc_grad(X, y, theta, 0.05, 500)
+print(theta_exact, theta_hat)
+
+# Pédiction : prédire le salaire d'une personne ayant 15.4 années d'expérience
+model(np.array([[15.4, 1]]), theta_exact)
+model(np.array([[15.4, 1]]), theta_hat)
+
+# Visualisation
+fig, (ax1,ax2) = plt.subplots(1,2,figsize=(14,8))
+ax1.scatter(x,y)
+ax1.plot(x,model(X, theta_hat), c='green')
+ax1.plot(x,model(X, theta_exact), c='red')
+ax2.plot(couts)
+plt.show()
