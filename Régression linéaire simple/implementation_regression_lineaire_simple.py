a58f64eaadc1acc7a5eed54600b3f53b7924a3d0
louiskuhn <kuhnlouis@gmail.com>
2021-11-17 11:35:04 +0100

    regression lineaire simple

---
+++ Régression linéaire simple/implementation_regression_lineaire_simple.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Charger les données, créer la matrice X et le vecteur y puis les afficher 
+
+# Matrice X : oublier pas d'ajouter la colonne de 1 !
+X = 
+
+# Initialisation theta
+theta = 
+
+# Modèle
+def model(X,theta):
+    return 
+
+# Fonction de coût
+def cout(X,y,theta):
+    return 
+    
+# Calcul du gradient
+def grad(X,y,theta):
+    return 
+    
+# Descente de gradient
+def desc_grad(X,y,theta,alpha=0.01,n_max=1000):
+
+
+# Solution exacte au problème
+def sol_exacte(X,y):
+    return
